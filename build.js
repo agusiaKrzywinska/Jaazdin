33cb169ff8ec745f298c6dae0c1bdbb72fc2f1c6
Malcolm Keefe <Mkeefeus@gmail.com>
2024-11-05 14:43:55 -0500

    Add and run prettier formatter

--- build.js
+++ build.js
@@ -3,88 +3,105 @@ const fs = require("fs");
 
 const EXCLUDED_FOLDERS = ["node_modules", ".git", ".vscode"];
 
-const FOLDERS = fs.readdirSync("./").filter((file) => fs.lstatSync(file).isDirectory() && !EXCLUDED_FOLDERS.includes(file));
+const FOLDERS = fs
+  .readdirSync("./")
+  .filter(
+    (file) =>
+      fs.lstatSync(file).isDirectory() && !EXCLUDED_FOLDERS.includes(file),
+  );
 
 const getSort = (folder) => {
-    switch (folder) {
-        case "subclass":
-            return (a, b) => {
-                return a.className.localeCompare(b.className) || a.name.localeCompare(b.name);
-            };
-        case "subclassFeature":
-            return (a, b) => {
-                return (
-                    a.className.localeCompare(b.className) || a.subclassShortName.localeCompare(b.subclassShortName) || a.level - b.level
-                );
-            };
-        case "classFeature":
-            return (a, b) => {
-                return a.className.localeCompare(b.className) || a.level - b.level;
-            };
-        default:
-            return (a, b) => {
-                return a.name.localeCompare(b.name);
-            };
-    }
+  switch (folder) {
+    case "subclass":
+      return (a, b) => {
+        return (
+          a.className.localeCompare(b.className) || a.name.localeCompare(b.name)
+        );
+      };
+    case "subclassFeature":
+      return (a, b) => {
+        return (
+          a.className.localeCompare(b.className) ||
+          a.subclassShortName.localeCompare(b.subclassShortName) ||
+          a.level - b.level
+        );
+      };
+    case "classFeature":
+      return (a, b) => {
+        return a.className.localeCompare(b.className) || a.level - b.level;
+      };
+    default:
+      return (a, b) => {
+        return a.name.localeCompare(b.name);
+      };
+  }
 };
 
 prompt.start();
 
 prompt.get(
-    {
-        properties: {
-            version: {
-                description: "Enter new version number",
-                type: "string",
-                pattern: /^\d+\.\d+\.\d+$/,
-                message: "Version must be in the format x.y.z where x, y, and z are numbers",
-                required: true,
-            },
-        },
+  {
+    properties: {
+      version: {
+        description: "Enter new version number",
+        type: "string",
+        pattern: /^\d+\.\d+\.\d+$/,
+        message:
+          "Version must be in the format x.y.z where x, y, and z are numbers",
+        required: true,
+      },
     },
-    (err, result) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        const version = result.version;
-        const newJson = {
-            $schema: "https://raw.githubusercontent.com/TheGiddyLimit/5etools-utils/master/schema/brew-fast/homebrew.json",
-        };
-        const _meta = {
-            sources: [
-                {
-                    json: "TftJC",
-                    abbreviation: "TftJC",
-                    full: "Tales from the Jaazdin Collective",
-                    authors: ["Nicholas McCarthy", "Adam Henderson"],
-                    convertedBy: ["Agusia Krzywinska", "Adam Henderson"],
-                    version: version,
-                },
-            ],
-            edition: "classic",
-            dateAdded: 0,
-            dateLastModified: Math.round(Date.now() / 1000),
-        };
-        newJson["_meta"] = _meta;
-        FOLDERS.forEach((folder) => {
-            newJson[folder] = newJson[folder] || [];
-            const jsonFiles = fs.readdirSync(`./${folder}`, { recursive: true }).filter((file) => file.endsWith(".json"));
-            jsonFiles.forEach((file) => {
-                const fileContent = fs.readFileSync(`./${folder}/${file}`);
-                const obj = JSON.parse(fileContent);
-                newJson[folder].push(obj);
-            });
-            newJson[folder].sort(getSort(folder));
-        });
-        const newJsonContent = JSON.stringify(newJson, null, 4);
-        fs.writeFile("./Tales_from_the_Jaazdin_Collective.json", newJsonContent, (err) => {
-            if (err) {
-                console.error(`Error writing file tftjc.json: ${err}`);
-            } else {
-                console.log(`File tftjc.json created successfully.`);
-            }
-        });
+  },
+  (err, result) => {
+    if (err) {
+      console.error(err);
+      return;
     }
+
+    const version = result.version;
+    const newJson = {
+      $schema:
+        "https://raw.githubusercontent.com/TheGiddyLimit/5etools-utils/master/schema/brew-fast/homebrew.json",
+    };
+    const _meta = {
+      sources: [
+        {
+          json: "TftJC",
+          abbreviation: "TftJC",
+          full: "Tales from the Jaazdin Collective",
+          authors: ["Nicholas McCarthy", "Adam Henderson"],
+          convertedBy: ["Agusia Krzywinska", "Adam Henderson"],
+          version: version,
+        },
+      ],
+      edition: "classic",
+      dateAdded: 0,
+      dateLastModified: Math.round(Date.now() / 1000),
+    };
+    newJson["_meta"] = _meta;
+    FOLDERS.forEach((folder) => {
+      newJson[folder] = newJson[folder] || [];
+      const jsonFiles = fs
+        .readdirSync(`./${folder}`, { recursive: true })
+        .filter((file) => file.endsWith(".json"));
+      jsonFiles.forEach((file) => {
+        const fileContent = fs.readFileSync(`./${folder}/${file}`);
+        const obj = JSON.parse(fileContent);
+        newJson[folder].push(obj);
+      });
+      newJson[folder].sort(getSort(folder));
+    });
+    const newJsonContent = JSON.stringify(newJson, null, 4);
+    fs.writeFile(
+      "./Tales_from_the_Jaazdin_Collective.json",
+      newJsonContent,
+      (err) => {
+        if (err) {
+          console.error(`Error writing file tftjc.json: ${err}`);
+        } else {
+          console.log(`File tftjc.json created successfully.`);
+        }
+      },
+    );
+  },
 );
--- destructure.js
+++ destructure.js
@@ -3,113 +3,122 @@ const fs = require("fs");
 
 //https://wiki.tercept.net/en/Homebrew/TableOfReference
 
-const TOP_FOLDERS = Object.keys(data).filter((key) => key !== "_meta" && key !== "$schema");
+const TOP_FOLDERS = Object.keys(data).filter(
+  (key) => key !== "_meta" && key !== "$schema",
+);
 
 const SPELL_SCHOOLS = {
-    A: "Abjuration",
-    C: "Conjuration",
-    D: "Divination",
-    E: "Enchantment",
-    I: "Illusion",
-    N: "Necromancy",
-    T: "Transmutation",
-    V: "Evocation",
+  A: "Abjuration",
+  C: "Conjuration",
+  D: "Divination",
+  E: "Enchantment",
+  I: "Illusion",
+  N: "Necromancy",
+  T: "Transmutation",
+  V: "Evocation",
 };
 
 const ITEM_TYPES = {
-    A: "Ammunition",
-    AF: "Ammunition (futuristic)",
-    AT: "Artisan Tool",
-    EM: "Eldritch Machine",
-    EXP: "Explosive",
-    G: "Adventuring Gear",
-    GS: "Gaming Set",
-    LA: "Light Armor",
-    INS: "Instrument",
-    M: "Melee Weapon",
-    MA: "Medium Armor",
-    MNT: "Mount",
-    GV: "Generic Variant",
-    P: "Potion",
-    R: "Ranged Weapon",
-    RD: "Rod",
-    RG: "Ring",
-    S: "Shield",
-    SC: "Scroll",
-    SCF: "Spellcasting Focus",
-    OTH: "Other",
-    T: "Tool",
-    TAH: "Tack and Harness",
-    TG: "Trade Good",
-    $: "Treasure",
-    VEH: "Vehicle (land)",
-    SHP: "Vehicle (water)",
-    AIR: "Vehicle (air)",
-    WD: "Wand",
+  A: "Ammunition",
+  AF: "Ammunition (futuristic)",
+  AT: "Artisan Tool",
+  EM: "Eldritch Machine",
+  EXP: "Explosive",
+  G: "Adventuring Gear",
+  GS: "Gaming Set",
+  LA: "Light Armor",
+  INS: "Instrument",
+  M: "Melee Weapon",
+  MA: "Medium Armor",
+  MNT: "Mount",
+  GV: "Generic Variant",
+  P: "Potion",
+  R: "Ranged Weapon",
+  RD: "Rod",
+  RG: "Ring",
+  S: "Shield",
+  SC: "Scroll",
+  SCF: "Spellcasting Focus",
+  OTH: "Other",
+  T: "Tool",
+  TAH: "Tack and Harness",
+  TG: "Trade Good",
+  $: "Treasure",
+  VEH: "Vehicle (land)",
+  SHP: "Vehicle (water)",
+  AIR: "Vehicle (air)",
+  WD: "Wand",
 };
 
 const sanitizeFileName = (fileName) => {
-    return fileName.replace(/[<>:"\/\\|?*]/g, "");
+  return fileName.replace(/[<>:"\/\\|?*]/g, "");
 };
 
 const writeToFile = (fileName, obj) => {
-    const path = fileName.split("/");
-    const sanitizedFileName = sanitizeFileName(path.pop());
-    const sanitizedPath = path.join("/");
-    const fileContent = JSON.stringify(obj, null, 4);
+  const path = fileName.split("/");
+  const sanitizedFileName = sanitizeFileName(path.pop());
+  const sanitizedPath = path.join("/");
+  const fileContent = JSON.stringify(obj, null, 4);
 
-    fs.writeFile(`${sanitizedPath}/${sanitizedFileName}`, fileContent, (err) => {
-        if (err) {
-            console.log(`Error writing file ${sanitizedFileName}: ${err}`);
-        }
-    });
+  fs.writeFile(`${sanitizedPath}/${sanitizedFileName}`, fileContent, (err) => {
+    if (err) {
+      console.log(`Error writing file ${sanitizedFileName}: ${err}`);
+    }
+  });
 };
 
 TOP_FOLDERS.forEach((key) => {
-    fs.existsSync(`./${key}`) || fs.mkdirSync(`./${key}`);
-    data[key].forEach((obj) => {
-        let filepath;
-        switch (key) {
-            case "reward":
-                fs.existsSync(`./reward/${obj.type}`) || fs.mkdirSync(`./reward/${obj.type}`);
-                filepath = `./reward/${obj.type}/${obj.name}.json`;
-                break;
+  fs.existsSync(`./${key}`) || fs.mkdirSync(`./${key}`);
+  data[key].forEach((obj) => {
+    let filepath;
+    switch (key) {
+      case "reward":
+        fs.existsSync(`./reward/${obj.type}`) ||
+          fs.mkdirSync(`./reward/${obj.type}`);
+        filepath = `./reward/${obj.type}/${obj.name}.json`;
+        break;
 
-            case "item":
-                const type = ITEM_TYPES[obj.type] || "Undefined";
-                fs.existsSync(`./item/${type}`) || fs.mkdirSync(`./item/${type}`);
-                filepath = `./item/${type}/${obj.name}.json`;
-                break;
+      case "item":
+        const type = ITEM_TYPES[obj.type] || "Undefined";
+        fs.existsSync(`./item/${type}`) || fs.mkdirSync(`./item/${type}`);
+        filepath = `./item/${type}/${obj.name}.json`;
+        break;
 
-            case "spell":
-                fs.existsSync(`./spell/${obj.level}`) || fs.mkdirSync(`./spell/${obj.level}`);
-                const school = SPELL_SCHOOLS[obj.school] || "Other";
-                fs.existsSync(`./spell/${obj.level}/${school}`) || fs.mkdirSync(`./spell/${obj.level}/${school}`);
-                filepath = `./spell/${obj.level}/${school}/${obj.name}.json`;
-                break;
+      case "spell":
+        fs.existsSync(`./spell/${obj.level}`) ||
+          fs.mkdirSync(`./spell/${obj.level}`);
+        const school = SPELL_SCHOOLS[obj.school] || "Other";
+        fs.existsSync(`./spell/${obj.level}/${school}`) ||
+          fs.mkdirSync(`./spell/${obj.level}/${school}`);
+        filepath = `./spell/${obj.level}/${school}/${obj.name}.json`;
+        break;
 
-            case "subrace":
-                fs.existsSync(`./subrace/${obj.raceName}`) || fs.mkdirSync(`./subrace/${obj.raceName}`);
-                filepath = `./subrace/${obj.raceName}/${obj.name}.json`;
-                break;
+      case "subrace":
+        fs.existsSync(`./subrace/${obj.raceName}`) ||
+          fs.mkdirSync(`./subrace/${obj.raceName}`);
+        filepath = `./subrace/${obj.raceName}/${obj.name}.json`;
+        break;
 
-            case "subclass":
-                fs.existsSync(`./subclass/${obj.className}`) || fs.mkdirSync(`./subclass/${obj.className}`);
-                filepath = `./subclass/${obj.className}/${obj.name}.json`;
-                break;
+      case "subclass":
+        fs.existsSync(`./subclass/${obj.className}`) ||
+          fs.mkdirSync(`./subclass/${obj.className}`);
+        filepath = `./subclass/${obj.className}/${obj.name}.json`;
+        break;
 
-            case "subclassFeature":
-                fs.existsSync(`./subclassFeature/${obj.subclassShortName}`) || fs.mkdirSync(`./subclassFeature/${obj.subclassShortName}`);
-                filepath = `./subclassFeature/${obj.subclassShortName}/${obj.name}.json`;
-                break;
-            case "classFeature":
-                fs.existsSync(`./classFeature/${obj.className}`) || fs.mkdirSync(`./classFeature/${obj.className}`);
-                filepath = `./classFeature/${obj.className}/${obj.level}. ${obj.name}.json`;
-                break;
-            default:
-                filepath = `./${key}/${obj.name}.json`;
-                break;
-        }
-        writeToFile(filepath, obj);
-    });
+      case "subclassFeature":
+        fs.existsSync(`./subclassFeature/${obj.subclassShortName}`) ||
+          fs.mkdirSync(`./subclassFeature/${obj.subclassShortName}`);
+        filepath = `./subclassFeature/${obj.subclassShortName}/${obj.name}.json`;
+        break;
+      case "classFeature":
+        fs.existsSync(`./classFeature/${obj.className}`) ||
+          fs.mkdirSync(`./classFeature/${obj.className}`);
+        filepath = `./classFeature/${obj.className}/${obj.level}. ${obj.name}.json`;
+        break;
+      default:
+        filepath = `./${key}/${obj.name}.json`;
+        break;
+    }
+    writeToFile(filepath, obj);
+  });
 });
