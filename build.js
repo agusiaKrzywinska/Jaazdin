afe4d17a7cc72d0ac78853bb13123b92f3ab1d64
Malcolm Keefe <Mkeefeus@gmail.com>
2025-07-05 19:51:29 -0400

    Update build script with --skip-version option

--- build.js
+++ build.js
@@ -38,77 +38,84 @@ const getSort = (folder) => {
   }
 };
 
-prompt.start();
+const updateJson = (version) => {
+  const newJson = {
+    $schema:
+      "https://raw.githubusercontent.com/TheGiddyLimit/5etools-utils/master/schema/brew-fast/homebrew.json",
+  };
+  const _meta = {
+    sources: [
+      {
+        json: "TftJC",
+        abbreviation: "TftJC",
+        full: "Tales from the Jaazdin Collective",
+        authors: ["Nicholas McCarthy", "Adam Henderson"],
+        convertedBy: ["Agusia Krzywinska", "Adam Henderson", "Malcolm Keefe"],
+        version: version,
+      },
+    ],
+    edition: "classic",
+    dateAdded: 0,
+    dateLastModified: Math.round(Date.now() / 1000),
+    optionalFeatureTypes: {
+      "FS:W": "Fighting Style: Warden",
+      TECH: "Technique",
+      ARC: "Arcana",
+      MOD: "Firearm Modification",
+    },
+  };
+  newJson["_meta"] = _meta;
+  FOLDERS.forEach((folder) => {
+    newJson[folder] = newJson[folder] || [];
+    const jsonFiles = fs
+      .readdirSync(`./${folder}`, { recursive: true })
+      .filter((file) => file.endsWith(".json"));
+    jsonFiles.forEach((file) => {
+      const fileContent = fs.readFileSync(`./${folder}/${file}`);
+      const obj = JSON.parse(fileContent);
+      newJson[folder].push(obj);
+    });
+    newJson[folder].sort(getSort(folder));
+  });
+  const newJsonContent = JSON.stringify(newJson, null, 4);
+  fs.writeFile(
+    "./Tales_from_the_Jaazdin_Collective.json",
+    newJsonContent,
+    (err) => {
+      if (err) {
+        console.error(`Error writing file tftjc.json: ${err}`);
+      } else {
+        console.log(`File tftjc.json created successfully.`);
+      }
+    },
+  );
+};
 
-prompt.get(
-  {
-    properties: {
-      version: {
-        description: `Enter new version number (Current: ${josnData._meta.sources[0].version})`,
-        type: "string",
-        pattern: /^\d+\.\d+\.\d+$/,
-        message:
-          "Version must be in the format x.y.z where x, y, and z are numbers",
-        required: true,
+if (process.argv.includes("-s") || process.argv.includes("--skip-prompt")) {
+  updateJson(jsonData._meta.sources[0].version);
+} else {
+  prompt.start();
+
+  prompt.get(
+    {
+      properties: {
+        version: {
+          description: `Enter new version number (Current: ${josnData._meta.sources[0].version})`,
+          type: "string",
+          pattern: /^\d+\.\d+\.\d+$/,
+          message:
+            "Version must be in the format x.y.z where x, y, and z are numbers",
+          required: true,
+        },
       },
     },
-  },
-  (err, result) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
 
-    const version = result.version;
-    const newJson = {
-      $schema:
-        "https://raw.githubusercontent.com/TheGiddyLimit/5etools-utils/master/schema/brew-fast/homebrew.json",
-    };
-    const _meta = {
-      sources: [
-        {
-          json: "TftJC",
-          abbreviation: "TftJC",
-          full: "Tales from the Jaazdin Collective",
-          authors: ["Nicholas McCarthy", "Adam Henderson"],
-          convertedBy: ["Agusia Krzywinska", "Adam Henderson", "Malcolm Keefe"],
-          version: version,
-        },
-      ],
-      edition: "classic",
-      dateAdded: 0,
-      dateLastModified: Math.round(Date.now() / 1000),
-      optionalFeatureTypes: {
-        "FS:W": "Fighting Style: Warden",
-        TECH: "Technique",
-        ARC: "Arcana",
-        MOD: "Firearm Modification",
-      },
-    };
-    newJson["_meta"] = _meta;
-    FOLDERS.forEach((folder) => {
-      newJson[folder] = newJson[folder] || [];
-      const jsonFiles = fs
-        .readdirSync(`./${folder}`, { recursive: true })
-        .filter((file) => file.endsWith(".json"));
-      jsonFiles.forEach((file) => {
-        const fileContent = fs.readFileSync(`./${folder}/${file}`);
-        const obj = JSON.parse(fileContent);
-        newJson[folder].push(obj);
-      });
-      newJson[folder].sort(getSort(folder));
-    });
-    const newJsonContent = JSON.stringify(newJson, null, 4);
-    fs.writeFile(
-      "./Tales_from_the_Jaazdin_Collective.json",
-      newJsonContent,
-      (err) => {
-        if (err) {
-          console.error(`Error writing file tftjc.json: ${err}`);
-        } else {
-          console.log(`File tftjc.json created successfully.`);
-        }
-      },
-    );
-  },
-);
+      updateJson(result.version);
+    },
+  );
+}
